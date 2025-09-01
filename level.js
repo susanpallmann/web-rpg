899f95af062d68f7bd41bc84215755bfaad813c1
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-09-01 13:34:29 -0400

    Update level.js

--- level.js
+++ level.js
@@ -1,21 +1,75 @@
+class Monster {
+  constructor() {
+    this.health = 10;
+    this.defense = 1;
+    this.damage = 1;
+  }
+
+  takeDamage(baseDamage) {
+    this.health =  this.health - (baseDamage - this.defense);
+  }
+  
+  dealDamage() {
+    return this.damage;
+  }
+}
+
+class Player {
+  constructor() {
+    this.health = 25;
+    this.defense = 3;
+    this.damage = 5;
+  }
+
+  takeDamage(baseDamage) {
+    this.health =  this.health - (baseDamage - this.defense);
+  }
+  
+  dealDamage() {
+    return this.damage;
+  }
+}
+
 class Battle {
-  constructor (turnNumber, maxTurns, turnFrequency) {
+  constructor (turnNumber, maxTurns, turnFrequency, player) {
+    this.player = player;
     this.turnNumber = turnNumber;
     this.maxTurns = maxTurns;
     this.turnFrequency = turnFrequency;
     this.battleTurnInterval;
     this.startBattleInterval();
+    this.monster = new Monster();
   }
 
   startBattleInterval() {
     this.battleTurnInterval = setInterval(() => {
 
       if (this.turnNumber < this.maxTurns) {
+        
         this.turnNumber++;
-        console.log('Turn interval increased.');
+        
+        console.log(`Turn ${this.turnNumber} begins.`);
+        
+        console.log(`Player has ${this.player.health} health.`);
+        console.log(`Monster has ${this.monster.health} health.`);
+
+        let playerDamageDealt = this.player.dealDamage();
+        let monsterDamageDealt = this.monster.dealDamage();
+
+        console.log(`Player dealt ${playerDamageDealt} damage.`);
+        console.log(`Monster dealt ${monsterDamageDealt} damage.`);
+
+        this.monster.takeDamage(playerDamageDealt);
+        this.player.takeDamage(monsterDamageDealt);
+        
+        console.log(`Player now has ${this.player.health} health.`);
+        console.log(`Monster now has ${this.monster.health} health.`);
+        
       } else {
+        
         this.endBattleInterval();
-        console.log('Turn interval finshed.');
+        console.log('Battle ran out of turns and ended.');
+        
       }
       
     }, this.turnFrequency);
@@ -26,7 +80,7 @@ class Battle {
   }
 }
 
-
 $(document).ready(function () {
-  let newBattle = new Battle(0, 5, 1000);
+  let newPlayer = new Player();
+  let newBattle = new Battle(0, 5, 1000, newPlayer);
 });
