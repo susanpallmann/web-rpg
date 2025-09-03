2e372526e4ce5c3be5bccc878a7cd176336eece6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2025-09-03 18:14:07 -0400

    Update stage.js

--- stage.js
+++ stage.js
@@ -1,10 +1,23 @@
-class Battle {
+import {regions, leveling, battles} from 'setup.js';
+
+class Stage {
   constructor(stage) {
     this.stageName = stage.name;
     this.level = stage.level;
     this.monsters = stage.monsters;
     this.bosses = stage.bosses;
     this.hasBoss = stage.bosses.length > 0;
+    this.numBattles = 0;
+  }
+
+  createBattle() {
+    // Determine if this is to be a boss battle
+    // Determine the monster types
+  }
+}
+
+class Battle {
+  constructor(battleType, monsterTypes) {
     this.turnInterval;
     this.numTurns = 0;
   }
@@ -27,6 +40,17 @@ class Battle {
     this.numTurns++;
   }
 };
+
+// Function to roll for an outcome given the chance (num) of that outcome
+// Returns true if outcome occurs, and false if not
+function roll(num) {
+  const randomNum = Math.floor(Math.random() * 100) + 1;
+  if (randomNum > (1/num)) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 $(document).ready(function() {
   let battle = new Battle(regions.mycanidMeadows.stages.portobelloPrairie);
